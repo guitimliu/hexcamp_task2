4476b960f5fd900a6677cb1dc3bb4829fe13acc1
Gui <guitimliu@gmail.com>
2024-06-03 00:39:53 +0800

    finish Works

--- src/pages/works/index.tsx
+++ src/pages/works/index.tsx
@@ -1,9 +1,14 @@
-// Home.jsx
+// Works.jsx
 import React, { useState } from 'react';
-import Pagination from '../../components/pagination';
+import Pagination from '../../components/Pagination';
 
 function Works() {
   const [ showPostID, setShowPostID ] = useState<number>(0);
+
+  const showPost = (id: number) => {
+    setShowPostID(id)
+    id > 0 ? document.body.style.overflow = 'hidden' : document.body.style.overflow = 'auto'
+  }
 
   return (
     <>
@@ -39,9 +44,9 @@ function Works() {
       </div>
       <div className="container mx-auto px-2 md:px-0 mb-10 md:mb-20">
         <ul className="flex justify-between gap-y-20 flex-wrap mb-12">
-            <li className="w-full md:w-[48%]">
-              <div onClick={() => setShowPostID(1)} className="cursor-pointer">
-                <img className="rounded-t-xl mb-4 max-w-full" src="https://fakeimg.pl/636x400/200" />
+            <li className="w-full md:w-[49.5%]">
+              <div onClick={() => showPost(1)} className="cursor-pointer">
+                <img className="rounded-t-xl mb-4 w-full" src="https://fakeimg.pl/636x400/200" />
                 <div className="pb-4">
                   <h2 className="text-xl font-bold mb-1">星際旅行訂票平台</h2>
                   <p className="text-gray-400">悠遊宇宙的夢想，從這裡開始實現</p>
@@ -53,9 +58,9 @@ function Works() {
                 </ul>
               </div>
             </li>
-            <li className="w-full md:w-[48%]">
-              <div>
-                <img className="rounded-t-xl mb-4 max-w-full" src="https://fakeimg.pl/636x400/200" />
+            <li className="w-full md:w-[49.5%]">
+              <div onClick={() => showPost(2)} className="cursor-pointer">
+                <img className="rounded-t-xl mb-4 w-full" src="https://fakeimg.pl/636x400/200" />
                 <div className="pb-4">
                   <h2 className="text-xl font-bold mb-1">理財App</h2>
                   <p className="text-gray-400">連動帳戶與行動支付，讓 AI 提供您最好的理財建議</p>
@@ -67,9 +72,9 @@ function Works() {
                 </ul>
               </div>
             </li>
-            <li className="w-full md:w-[48%]">
-              <div>
-                <img className="rounded-t-xl mb-4 max-w-full" src="https://fakeimg.pl/636x400/200" />
+            <li className="w-full md:w-[49.5%]">
+              <div onClick={() => showPost(3)} className="cursor-pointer">
+                <img className="rounded-t-xl mb-4 w-full" src="https://fakeimg.pl/636x400/200" />
                 <div className="pb-4">
                   <h2 className="text-xl font-bold mb-1">醫美診所官網</h2>
                   <p className="text-gray-400">永保青春的秘密，從粹究開始</p>
@@ -81,9 +86,9 @@ function Works() {
                 </ul>
               </div>
             </li>
-            <li className="w-full md:w-[48%]">
-              <div>
-                <img className="rounded-t-xl mb-4 max-w-full" src="https://fakeimg.pl/636x400/200" />
+            <li className="w-full md:w-[49.5%]">
+              <div onClick={() => showPost(4)} className="cursor-pointer">
+                <img className="rounded-t-xl mb-4 w-full" src="https://fakeimg.pl/636x400/200" />
                 <div className="pb-4">
                   <h2 className="text-xl font-bold mb-1">美美美早餐店 POS 機 UI Design</h2>
                   <p className="text-gray-400">訂單送單一目瞭然，自動報表分析好輕鬆</p>
@@ -95,9 +100,9 @@ function Works() {
                 </ul>
               </div>
             </li>
-            <li className="w-full md:w-[48%]">
-              <div>
-                <img className="rounded-t-xl mb-4 max-w-full" src="https://fakeimg.pl/636x400/200" />
+            <li className="w-full md:w-[49.5%]">
+              <div onClick={() => showPost(5)} className="cursor-pointer">
+                <img className="rounded-t-xl mb-4 w-full" src="https://fakeimg.pl/636x400/200" />
                 <div className="pb-4">
                   <h2 className="text-xl font-bold mb-1">電影院訂票系統</h2>
                   <p className="text-gray-400">三步驟完成訂票，電腦手機都支援</p>
@@ -109,9 +114,9 @@ function Works() {
                 </ul>
               </div>
             </li>
-            <li className="w-full md:w-[48%]">
-              <div>
-                <img className="rounded-t-xl mb-4 max-w-full" src="https://fakeimg.pl/636x400/200" />
+            <li className="w-full md:w-[49.5%]">
+              <div onClick={() => showPost(6)} className="cursor-pointer">
+                <img className="rounded-t-xl mb-4 w-full" src="https://fakeimg.pl/636x400/200" />
                 <div className="pb-4">
                   <h2 className="text-xl font-bold mb-1">星際旅行訂票平台</h2>
                   <p className="text-gray-400">悠遊宇宙的夢想，從這裡開始實現</p>
@@ -130,44 +135,46 @@ function Works() {
       </div>
       {
         showPostID !== 0 ?
-        <div className="flex justify-center absolute top-0 left-0 w-full min-h-full">
+        <div className="flex justify-center fixed top-0 left-0 w-full h-full">
           <div 
             className="absolute top-0 left-0 z-20 w-full h-full bg-black opacity-75"
-            onClick={() => setShowPostID(0)}
+            onClick={() => showPost(0)}
           />
-          <div className="relative bg-white max-w-[1200px] rounded-xl my-20 z-20">
-              <div className="pt-10 pr-10">
-                <svg onClick={() => setShowPostID(0)}  className="cursor-pointer ms-auto" width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M10.9993 13.3337L2.83268 21.5003C2.52713 21.8059 2.13824 21.9587 1.66602 21.9587C1.19379 21.9587 0.804905 21.8059 0.499349 21.5003C0.193793 21.1948 0.0410156 20.8059 0.0410156 20.3337C0.0410156 19.8614 0.193793 19.4725 0.499349 19.167L8.66602 11.0003L0.499349 2.83366C0.193793 2.5281 0.0410156 2.13921 0.0410156 1.66699C0.0410156 1.19477 0.193793 0.805881 0.499349 0.500326C0.804905 0.19477 1.19379 0.0419922 1.66602 0.0419922C2.13824 0.0419922 2.52713 0.19477 2.83268 0.500326L10.9993 8.66699L19.166 0.500326C19.4716 0.19477 19.8605 0.0419922 20.3327 0.0419922C20.8049 0.0419922 21.1938 0.19477 21.4993 0.500326C21.8049 0.805881 21.9577 1.19477 21.9577 1.66699C21.9577 2.13921 21.8049 2.5281 21.4993 2.83366L13.3327 11.0003L21.4993 19.167C21.8049 19.4725 21.9577 19.8614 21.9577 20.3337C21.9577 20.8059 21.8049 21.1948 21.4993 21.5003C21.1938 21.8059 20.8049 21.9587 20.3327 21.9587C19.8605 21.9587 19.4716 21.8059 19.166 21.5003L10.9993 13.3337Z" fill="black"/>
-                </svg>
-              </div>
-              <div className="p-10">
-                <h2 className="text-[28px] text-center font-bold mb-2">星際旅行訂票平台</h2>
-                <h3 className="text-center text-gray-400 mb-10">悠遊宇宙的夢想，從這裡開始實現</h3>
-                <div className="md:flex justify-between items-center">
-                  <div className="md:w-6/12 mb-8">
-                    <p>STAR TRAVEL 為 KK 公司宇宙旅行的購票網站。</p>
-                    <p>主要從地球出發，目的地包含月球、火星、水星、木星；未來太空轉運站建置完畢，將再擴增目的地土星和金星。</p>
+          <div className="relative bg-white max-w-[1200px] h-3/4 overflow-hidden rounded-xl my-20 z-20">
+              <div className='overflow-auto w-full h-full'>
+                <div className="pt-10 pr-10">
+                  <svg onClick={() => showPost(0)}  className="cursor-pointer ms-auto" width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M10.9993 13.3337L2.83268 21.5003C2.52713 21.8059 2.13824 21.9587 1.66602 21.9587C1.19379 21.9587 0.804905 21.8059 0.499349 21.5003C0.193793 21.1948 0.0410156 20.8059 0.0410156 20.3337C0.0410156 19.8614 0.193793 19.4725 0.499349 19.167L8.66602 11.0003L0.499349 2.83366C0.193793 2.5281 0.0410156 2.13921 0.0410156 1.66699C0.0410156 1.19477 0.193793 0.805881 0.499349 0.500326C0.804905 0.19477 1.19379 0.0419922 1.66602 0.0419922C2.13824 0.0419922 2.52713 0.19477 2.83268 0.500326L10.9993 8.66699L19.166 0.500326C19.4716 0.19477 19.8605 0.0419922 20.3327 0.0419922C20.8049 0.0419922 21.1938 0.19477 21.4993 0.500326C21.8049 0.805881 21.9577 1.19477 21.9577 1.66699C21.9577 2.13921 21.8049 2.5281 21.4993 2.83366L13.3327 11.0003L21.4993 19.167C21.8049 19.4725 21.9577 19.8614 21.9577 20.3337C21.9577 20.8059 21.8049 21.1948 21.4993 21.5003C21.1938 21.8059 20.8049 21.9587 20.3327 21.9587C19.8605 21.9587 19.4716 21.8059 19.166 21.5003L10.9993 13.3337Z" fill="black"/>
+                  </svg>
+                </div>
+                <div className="p-10">
+                  <h2 className="text-[28px] text-center font-bold mb-2">星際旅行訂票平台</h2>
+                  <h3 className="text-center text-gray-400 mb-10">悠遊宇宙的夢想，從這裡開始實現</h3>
+                  <div className="md:flex justify-between items-center">
+                    <div className="md:w-6/12 mb-8">
+                      <p>STAR TRAVEL 為 KK 公司宇宙旅行的購票網站。</p>
+                      <p>主要從地球出發，目的地包含月球、火星、水星、木星；未來太空轉運站建置完畢，將再擴增目的地土星和金星。</p>
+                    </div>
+                    <ul className="border-t md:border-t-0 md:border-s border-gray-300 text-gray-400 text-center md:text-start ps-4 pt-8 md:pt-0">
+                      <li>設計原則：宇宙旅行、深色背景、強烈視覺效果</li>
+                      <li>開發方式：Bootstrap、Javascript、RWD</li>
+                    </ul>
                   </div>
-                  <ul className="border-t md:border-t-0 md:border-s border-gray-300 text-gray-400 text-center md:text-start ps-4 pt-8 md:pt-0">
-                    <li>設計原則：宇宙旅行、深色背景、強烈視覺效果</li>
-                    <li>開發方式：Bootstrap、Javascript、RWD</li>
+                </div>
+                <div className="bg-gray-50 px-2 md:px-0 py-10">
+                  <ul>
+                    <li className="mb-8">
+                      <img className="rounded-t-xl max-w-full mx-auto mb-2" src="https://fakeimg.pl/636x400/200" />
+                      <h3 className="text-center text-2xl font-bold mb-2">主視覺</h3>
+                      <p className="text-center">可直接設定日期、起迄星球、張數，快速查詢可購買的票券。</p>
+                    </li>
+                    <li>
+                      <img className="rounded-t-xl max-w-full mx-auto mb-2" src="https://fakeimg.pl/636x400/200" />
+                      <h3 className="text-center text-2xl font-bold mb-2">推薦查詢</h3>
+                      <p className="text-center">給予最快起飛，以及最熱門的景點推薦，協助使用者進行選擇。</p>
+                    </li>
                   </ul>
                 </div>
-              </div>
-              <div className="bg-gray-50 px-2 md:px-0 py-10">
-                <ul>
-                  <li className="mb-8">
-                    <img className="rounded-t-xl max-w-full mx-auto mb-2" src="https://fakeimg.pl/636x400/200" />
-                    <h3 className="text-center text-2xl font-bold mb-2">主視覺</h3>
-                    <p className="text-center">可直接設定日期、起迄星球、張數，快速查詢可購買的票券。</p>
-                  </li>
-                  <li>
-                    <img className="rounded-t-xl max-w-full mx-auto mb-2" src="https://fakeimg.pl/636x400/200" />
-                    <h3 className="text-center text-2xl font-bold mb-2">推薦查詢</h3>
-                    <p className="text-center">給予最快起飛，以及最熱門的景點推薦，協助使用者進行選擇。</p>
-                  </li>
-                </ul>
               </div>
           </div>
         </div>
